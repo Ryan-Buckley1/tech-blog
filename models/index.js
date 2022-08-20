1bdecdced67714e9d20a9d280e9213b6fd65e05b
Buckimus <ryan42buck@gmail.com>
2022-08-20 13:44:55 -0400

    added associations

--- models/index.js
+++ models/index.js
@@ -4,4 +4,22 @@ const Post = require("./Post");
 
 //associations
 
+//User-Post
+
+User.hasMany(Post);
+
+Post.belongsTo(User);
+
+//User-Comment
+
+User.hasMany(Comment);
+
+Comment.belongsTo(User);
+
+//Post-Comment
+
+Post.hasMany(Comment);
+
+Comment.belongsTo(Post);
+
 module.exports = { User, Comment, Post };
