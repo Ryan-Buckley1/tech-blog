be7c1bd12a66fa68ead87fb574b9170f039ee7c8
Buckimus <ryan42buck@gmail.com>
2022-08-25 16:14:10 -0400

    updated home routes and added dashboard routes to index

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,11 @@
-const { Post, User } = require("../models");
+const { Post, User, Comment } = require("../models");
 
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
   try {
     const allPosts = await Post.findAll({
-      attributes: ["id", "title", "postText"],
+      attributes: ["id", "title", "postText", "createdAt"],
     });
     const posts = allPosts.map((post) => post.get({ plain: true }));
     res.render("homepage", { posts, loggedIn: req.session.loggedIn });
@@ -17,6 +17,40 @@ router.get("/", async (req, res) => {
 // router.get("/", (req, res) => {
 //   res.render("homepage");
 // });
+
+router.get("/post/:id", async (req, res) => {
+  try {
+    const singlePost = await Post.findOne({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ["id", "postText", "title", "createdAt"],
+      include: [
+        {
+          model: Comment,
+          attributes: ["id", "comment_text", "postId", "userId", "created_at"],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        // {
+        //   model: User,
+        //   attributes: ["username"],
+        // },
+      ],
+    });
+    if (!singlePost) {
+      res.status(404).json({ message: "No post found with that id" });
+      return;
+    }
+    const post = singlePost.get({ plain: true });
+    res.render("single-post", { post, loggedIn: req.session.loggedIn });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+});
 
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
